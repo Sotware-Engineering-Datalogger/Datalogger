ec1c110af0af432dccf3452b9a9076c74f797a25
ldiy <lorenz_reclame@outlook.be>
2022-05-13 13:45:29 +0200

    Added tests for the sensor post requests

--- server-backend/tests/Feature/SensorDataTest.php
+++ server-backend/tests/Feature/SensorDataTest.php
@@ -45,7 +45,7 @@ class SensorDataTest extends TestCase
     }
 
     /**
-     * Test if a get request to /sensor/all is successful.
+     * Test if a get request to /sensor/light is successful.
      *
      * @return void
      */
@@ -56,4 +56,131 @@ class SensorDataTest extends TestCase
         $response->assertStatus(200);
     }
 
+    /**
+     * Test if new temperature data is stored in the database.
+     *
+     * @return void
+     */
+    public function test_store_temperature_data()
+    {
+        $response = $this->postJson('/sensor/temperature', [
+            'value' => '25'
+        ]);
+
+        $response->assertStatus(201)->assertExactJson([
+                'created' => true,
+        ]);
+    }
+
+    /**
+     * Test if new humidity data is stored in the database.
+     *
+     * @return void
+     */
+    public function test_store_humidity_data()
+    {
+        $response = $this->postJson('/sensor/humidity', [
+            'value' => '15'
+        ]);
+
+        $response->assertStatus(201)->assertExactJson([
+                'created' => true,
+        ]);
+    }
+
+    /**
+     * Test if new pressure data is stored in the database.
+     *
+     * @return void
+     */
+    public function test_store_pressure_data()
+    {
+        $response = $this->postJson('/sensor/pressure', [
+            'value' => '30'
+        ]);
+
+        $response->assertStatus(201)->assertExactJson([
+                'created' => true,
+        ]);
+    }
+
+    /**
+     * Test if new light data is stored in the database.
+     *
+     * @return void
+     */
+    public function test_store_light_data()
+    {
+        $response = $this->postJson('/sensor/light', [
+            'value' => '10'
+        ]);
+
+        $response->assertStatus(201)->assertExactJson([
+                'created' => true,
+        ]);
+    }
+
+    /**
+     * Test if a post request with invalid data is rejected.
+     *
+     * @return void
+     */
+    public function test_store_temperature_data_with_invalid_value()
+    {
+        $response = $this->postJson('/sensor/temperature', [
+            'value' => 'invalid'
+        ]);
+
+        $response->assertStatus(422)->assertExactJson([
+                'created' => false,
+        ]);
+    }
+
+    /**
+     * Test if a post request with invalid data is rejected.
+     *
+     * @return void
+     */
+    public function test_store_humidity_data_with_invalid_value()
+    {
+        $response = $this->postJson('/sensor/humidity', [
+            'value' => 'invalid'
+        ]);
+
+        $response->assertStatus(422)->assertExactJson([
+                'created' => false,
+        ]);
+    }
+
+    /**
+     * Test if a post request with invalid data is rejected.
+     *
+     * @return void
+     */
+    public function test_store_pressure_data_with_invalid_value()
+    {
+        $response = $this->postJson('/sensor/pressure', [
+            'value' => 'invalid'
+        ]);
+
+        $response->assertStatus(422)->assertExactJson([
+                'created' => false,
+        ]);
+    }
+
+    /**
+     * Test if a post request with invalid data is rejected.
+     *
+     * @return void
+     */
+    public function test_store_light_data_with_invalid_value()
+    {
+        $response = $this->postJson('/sensor/light', [
+            'value' => 'invalid'
+        ]);
+
+        $response->assertStatus(422)->assertExactJson([
+                'created' => false,
+        ]);
+    }
 }
